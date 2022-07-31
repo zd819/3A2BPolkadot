fbd3d7ea680aa81875b8e3f09f7d1c217e6886e1
zd819 <zd819@ic.ac.uk>
2022-07-31 05:01:06 +0100

    ERC20 Basic Token

--- erc20/lib.rs
+++ erc20/lib.rs
@@ -4,71 +4,230 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod erc20 {
+    use ink_storage::{
+        traits::SpreadAllocate,
+        Mapping,
+    };
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    #[ink(event)]
+        pub struct Transfer {
+        #[ink(topic)]
+        from: Option<AccountId>,
+        #[ink(topic)]
+        to: Option<AccountId>,
+        value: Balance,
+    }
+
+    //Approval event for 3rd party Access of User tokens
+    #[ink(event)]
+    pub struct Approval {
+        #[ink(topic)]
+        owner: AccountId,
+        #[ink(topic)]
+        spender: AccountId,
+        value: Balance,
+    }
+
+    /// Create storage for a simple ERC-20 contract.
     #[ink(storage)]
+    #[derive(SpreadAllocate)]
     pub struct Erc20 {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        /// Total token supply.
+        total_supply: Balance,
+        /// Mapping from owner to number of owned tokens.
+        balances: Mapping<AccountId, Balance>,
+        //Storage mapping for an owner and non-owner combination to an account balance
+        allowances: Mapping<(AccountId, AccountId), Balance>,
     }
 
+    /// Specify ERC-20 error type.
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+    /// Return if the balance cannot fulfill a request.
+    InsufficientBalance,
+    //Error declaration to return an error if the transfer request exceeds the account allowance
+    InsufficientAllowance,
+    }
+
+    /// Specify the ERC-20 result type.
+    pub type Result<T> = core::result::Result<T, Error>;
     impl Erc20 {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
+        /// Create a new ERC-20 contract with an initial supply.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(initial_supply: Balance) -> Self {
+            // Initialize mapping for the contract.
+            ink_lang::utils::initialize_contract(|contract| {
+                Self::new_init(contract, initial_supply)
+            })
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
+        /// Initialize the ERC-20 contract with the specified initial supply.
+        fn new_init(&mut self, initial_supply: Balance) {
+            let caller = Self::env().caller();
+            self.balances.insert(&caller, &initial_supply);
+            self.total_supply = initial_supply;
+            //Adding transfer event for initial contract supply amount using a from to none trasnfer event
+            Self::env().emit_event(Transfer {
+                from: None,
+                to: Some(caller),
+                value: initial_supply,
+              });
         }
 
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
+        /// Returns the total token supply.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn total_supply(&self) -> Balance {
+            self.total_supply
         }
 
-        /// Simply returns the current value of our `bool`.
+        /// Returns the account balance for the specified `owner`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn balance_of(&self, owner: AccountId) -> Balance {
+            self.balances.get(owner).unwrap_or_default()
         }
+
+        #[ink(message)]
+        pub fn transfer(&mut self, to: AccountId, value: Balance) -> Result<()> {
+        let from = self.env().caller();
+        self.transfer_from_to(&from, &to, value)
+        }
+
+        fn transfer_from_to(
+            &mut self,
+            from: &AccountId,
+            to: &AccountId,
+            value: Balance,
+         ) -> Result<()> {
+            let from_balance = self.balance_of_impl(from);
+            if from_balance < value {
+                return Err(Error::InsufficientBalance)
+            }
+        
+            self.balances.insert(from, &(from_balance - value));
+            let to_balance = self.balance_of_impl(to);
+            self.balances.insert(to, &(to_balance + value));
+            self.env().emit_event(Transfer {
+            from: Some(*from),
+            to: Some(*to),
+            value,
+            });
+            Ok(())
+         }
+
+        #[inline]
+        fn balance_of_impl(&self, owner: &AccountId) -> Balance {
+        self.balances.get(owner).unwrap_or_default()
+        }
+
+        // Approve() function to authorize a spender account to withdraw tokens from the caller's account 
+        // up to a maximum value
+        #[ink(message)]
+        pub fn approve(&mut self, spender: AccountId, value: Balance) -> Result<()> {
+        let owner = self.env().caller();
+        self.allowances.insert((&owner, &spender), &value);
+        self.env().emit_event(Approval {
+            owner,
+            spender,
+            value,
+        });
+        Ok(())
+        }
+
+        //Allowance() function to return the number of tokens 
+        //a spender is allowed to withdraw from the owner account
+        #[ink(message)]
+        pub fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
+        self.allowance_impl(&owner, &spender)
+        }
+
+        // Same as the allowance() function except that it uses references to 
+        // look up the token allowance in a more efficient way in WebAssembly
+        #[inline]
+        fn allowance_impl(&self, owner: &AccountId, spender: &AccountId) -> Balance {
+        self.allowances.get((owner, spender)).unwrap_or_default()
+        }
+        
+
+        /// Transfers tokens on the behalf of the `from` account to the `to account
+        #[ink(message)]
+        pub fn transfer_from(
+            &mut self,
+            from: AccountId,
+            to: AccountId,
+            value: Balance,
+        ) -> Result<()> {
+            let caller = self.env().caller();
+            let allowance = self.allowance_impl(&from, &caller);
+            if allowance < value {
+                return Err(Error::InsufficientAllowance)
+            }
+            self.transfer_from_to(&from, &to, value)?;
+            self.allowances
+                .insert((&from, &caller), &(allowance - value));
+            Ok(())
+            }
+           
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
+        #[cfg(test)]
+        mod tests {
         use super::*;
-
-        /// Imports `ink_lang` so we can use `#[ink::test]`.
+    
         use ink_lang as ink;
-
-        /// We test if the default constructor does its job.
+    
         #[ink::test]
-        fn default_works() {
-            let erc20 = Erc20::default();
-            assert_eq!(erc20.get(), false);
+        fn new_works() {
+            let contract = Erc20::new(777);
+            assert_eq!(contract.total_supply(), 777);
+        }
+    
+        #[ink::test]
+        fn balance_works() {
+            let contract = Erc20::new(100);
+            assert_eq!(contract.total_supply(), 100);
+            assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
+            assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 0);
         }
 
-        /// We test a simple use case of our contract.
+        //Add transfer test
+
+        //Transfer event test
         #[ink::test]
-        fn it_works() {
-            let mut erc20 = Erc20::new(false);
-            assert_eq!(erc20.get(), false);
-            erc20.flip();
-            assert_eq!(erc20.get(), true);
+        fn transfer_works() {
+        let mut erc20 = Erc20::new(100);
+        assert_eq!(erc20.balance_of(AccountId::from([0x0; 32])), 0);
+        assert_eq!(erc20.transfer(AccountId::from([0x0; 32]), 10), Ok(()));
+        assert_eq!(erc20.balance_of(AccountId::from([0x0; 32])), 10);
         }
+
+
+        //Test case for 3rd Party Transfers
+        #[ink::test]
+        fn transfer_from_works() {
+        let mut contract = Erc20::new(100);
+        assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
+        contract.approve(AccountId::from([0x1; 32]), 20);
+        contract.transfer_from(AccountId::from([0x1; 32]), AccountId::from([0x0; 32]), 10);
+        assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 10);
+        }
+
+        //Test for 3rd Party Allowance Calls
+        #[ink::test]
+        fn allowances_works() {
+        let mut contract = Erc20::new(100);
+        assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
+        contract.approve(AccountId::from([0x1; 32]), 200);
+        assert_eq!(contract.allowance(AccountId::from([0x1; 32]), AccountId::from([0x1; 32])), 200);
+
+        contract.transfer_from(AccountId::from([0x1; 32]), AccountId::from([0x0; 32]), 50);
+        assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 50);
+        assert_eq!(contract.allowance(AccountId::from([0x1; 32]), AccountId::from([0x1; 32])), 150);
+
+        contract.transfer_from(AccountId::from([0x1; 32]), AccountId::from([0x0; 32]), 100);
+        assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 50);
+        assert_eq!(contract.allowance(AccountId::from([0x1; 32]), AccountId::from([0x1; 32])), 150);
     }
+    
+}
 }
